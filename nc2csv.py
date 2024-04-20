dcd0be73ef0164ade1d60f7b897db2ad48081921
ttbilgin <ttbilgin@gmail.com>
2024-04-20 16:40:21 +0300

    Update nc2csv.py

--- nc2csv.py
+++ nc2csv.py
@@ -2,6 +2,11 @@ import argparse
 import netCDF4
 import xarray as xr
 import os
+import metpy.calc as mpcalc
+from metpy.calc import (mixing_ratio_from_relative_humidity, relative_humidity_from_dewpoint,
+                        thickness_hydrostatic)
+from metpy.cbook import get_test_data
+from metpy.units import units
 
 
 def replace_dots_with_commas(filename):
@@ -11,7 +16,21 @@ def replace_dots_with_commas(filename):
     with open(filename, 'w', encoding='ANSI') as file:
         file.write(text)
 
+def relative_humidity_calculate(dataset):
+    # relative humidity calculate
+    T = dataset.t2m * units.kelvin
+    Td = dataset.d2m * units.kelvin
+    rh = relative_humidity_from_dewpoint(T, Td)
+    dataset['TdegC']= T.metpy.convert_units('degC')
+    dataset['Td_degC']=Td.metpy.convert_units('degC')
+    dataset['rh']=rh
+    return dataset
 
+	
+def wind_speed_calculate(dataset):
+    # wind speed calculate
+    dataset['w_s'] = mpcalc.wind_speed(dataset.u10, dataset.v10)
+    return dataset
 
 
 
@@ -25,7 +44,11 @@ def main():
     # Define switch for comma or semicolon
     parser.add_argument('-tr', '--turkish_num_format', action='store_true', help='Use Turkish format for float/double numbers.')
 
+	# Define switch for relative humidity
+    parser.add_argument('-rh', '--relative_humidity', action='store_true', help='calculate relative humidity.')
 
+	# Define switch for wind speed
+    parser.add_argument('-ws', '--wind_speed', action='store_true', help='calculate wind speed.')
 
     # Parse command-line arguments
     args = parser.parse_args()
@@ -43,6 +66,13 @@ def main():
     
     netCDFdata = netCDF4.Dataset(args.input, mode='r')
     dataset = xr.open_dataset(xr.backends.NetCDF4DataStore(netCDFdata))
+	
+    if args.relative_humidity:
+        dataset=relative_humidity_calculate(dataset)
+
+    if args.wind_speed:
+        dataset=wind_speed_calculate(dataset)
+		
     #csv ye  aktarmak için
     df= dataset.to_dataframe()
     # Change the extension of the input file
@@ -52,6 +82,9 @@ def main():
     df.to_csv(new_input_file,sep=';')
     if args.turkish_num_format:
         replace_dots_with_commas(new_input_file)
+		
+
+		
 
 if __name__ == '__main__':
     main()
